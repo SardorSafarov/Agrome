d09faed2d0af2c8fb0b0e1f0ac94660375503fea
SardorSafarov <zomin000s@gmail.com>
2022-10-30 00:52:09 +0500

    login gone

---
+++ app/src/main/java/com/example/zamin/agrome/onBoarding/login/activity/SignInActivity.kt
@@ -0,0 +1,14 @@
+package com.example.zamin.agrome.onBoarding.login.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.zamin.agrome.databinding.ActivitySignInBinding
+
+class SignInActivity : AppCompatActivity() {
+    lateinit var binding:ActivitySignInBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/example/zamin/agrome/onBoarding/login/activity/SignUpActivity.kt
@@ -0,0 +1,14 @@
+package com.example.zamin.agrome.onBoarding.login.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.zamin.agrome.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+    lateinit var binding:ActivitySignUpBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
